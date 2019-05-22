8bc584a861307a22b41819034fc25f0368fcb9a6
Valentina Lipari <v.lipari@studenti.unina.it>
2019-05-22 11:24:56 +0200

    onPause() method of Activity Life cycle
    added onPause() method of Activity Life cycle that reset app when its state move in "onPause()".
    added logo

--- circularanimatedimg-example/src/main/java/it/valix93/circularanimatedimage/MainActivity.java
+++ circularanimatedimg-example/src/main/java/it/valix93/circularanimatedimage/MainActivity.java
@@ -1,5 +1,6 @@
 package it.valix93.circularanimatedimage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -25,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+    }
+
+
+    // when the app state is onPause, call finish() method.
+    @Override
+    protected void onPause(){
+        finish();
+        super.onPause();
+
     }
 
 }
